2ba64ac96b1b9cedf79b44aaad7a9662c4575a67
juaneea <jeescobar.17@est.ucab.edu.ve>
2020-11-18 23:27:36 -0400

    CRUD Categoria

---
+++ servicio/src/test/java/pruebaCategoriaTest.java
@@ -0,0 +1,56 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ucab.dsw.dtos.CategoriaDto;
+
+
+
+public class pruebaCategoriaTest
+{
+
+    @Test
+    public void addCategoriaTest() throws Exception
+    {
+        ucab.dsw.servicio.pruebaCategoria servicio = new ucab.dsw.servicio.pruebaCategoria();
+        CategoriaDto  CategoriaDto= new CategoriaDto();
+        CategoriaDto.setDescripcion( "Bismarck" );
+        CategoriaDto.setEstatus("A");
+        CategoriaDto resultado = servicio.addCategoria( CategoriaDto );
+        Assert.assertNotEquals( resultado.getId(), 1 );
+
+    }
+
+    @Test
+    public void changeCategoriaTest() throws Exception
+    {
+        ucab.dsw.servicio.pruebaCategoria servicio = new ucab.dsw.servicio.pruebaCategoria();
+        CategoriaDto  CategoriaDto= new CategoriaDto( 1);
+        CategoriaDto.setDescripcion( "Pedro" );
+        CategoriaDto.setEstatus("A");
+        CategoriaDto resultado = servicio.changeCategoria( CategoriaDto );
+        Assert.assertNotEquals( resultado.getId(), 0 );
+
+    }
+
+    @Test
+    public void deleteCategoriaTest() throws Exception
+    {
+        ucab.dsw.servicio.pruebaCategoria servicio = new ucab.dsw.servicio.pruebaCategoria();
+        CategoriaDto resultado = servicio.deleteCategoria( 2 );
+        Assert.assertNotEquals( resultado.getId(), 0 );
+
+    }
+
+    @Test
+    public void showCategoriaTest() throws Exception
+    {
+        ucab.dsw.servicio.pruebaCategoria servicio = new ucab.dsw.servicio.pruebaCategoria();
+        CategoriaDto  CategoriaDto= new CategoriaDto( 6);
+        CategoriaDto resultado = servicio.showCategoria( CategoriaDto.getId() );
+        Assert.assertEquals( resultado.getDescripcion(), "hola");
+
+    }
+
+
+}
