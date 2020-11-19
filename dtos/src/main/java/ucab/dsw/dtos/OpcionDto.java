542597e557d3fcbcbc7a88803275681fb4a836ad
Hguedez <57779032+Hguedez@users.noreply.github.com>
2020-11-19 18:58:25 -0400

    Dto Opcion y Tipo pregunta agregado

---
+++ dtos/src/main/java/ucab/dsw/dtos/OpcionDto.java
@@ -0,0 +1,37 @@
+package ucab.dsw.dtos;
+
+public class OpcionDto extends DtoBase{
+
+    public String getDescripcion()
+    {
+        return descripcion;
+    }
+
+    public void setDescripcion( String descripcion )
+    {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstatus()
+    {
+        return estatus;
+    }
+
+    public void setEstatus( String estatus )
+    {
+        this.estatus = estatus;
+    }
+
+    public PreguntaDto getPreguntaDto() {
+        return preguntaDto;
+    }
+
+    public void setPreguntaDto(PreguntaDto preguntaDto) {
+        this.preguntaDto = preguntaDto;
+    }
+
+    private String estatus;
+    private PreguntaDto preguntaDto;
+    private String descripcion;
+
+}
