6019b55a379565360918507de3104433ad8e531e
Hguedez <57779032+Hguedez@users.noreply.github.com>
2020-11-20 16:33:14 -0400

    Pregunta test

---
+++ servicio/src/test/java/Pregunta_Test.java
@@ -0,0 +1,58 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ucab.dsw.dtos.*;
+import ucab.dsw.entidades.Categoria;
+import ucab.dsw.entidades.Pregunta;
+import ucab.dsw.servicio.CategoriaORMWS;
+
+import java.util.List;
+
+public class Pregunta_Test {
+
+    @Test
+    public void addPregunta() throws Exception{
+        ucab.dsw.servicio.PreguntaORMWS servicio = new ucab.dsw.servicio.PreguntaORMWS();
+        PreguntaDto preguntaDto = new PreguntaDto();
+        preguntaDto.setPregunta( "Hola como estas?" );
+        preguntaDto.setFechacreacion( "2020/11/20" );
+        preguntaDto.setEstatus( "A" );
+        /*Tipo pregunta*/
+        TipoPreguntaDto tipoPregunta = new TipoPreguntaDto( 2);
+        preguntaDto.setTipoPreguntaDto( tipoPregunta );
+        /*Subcategoria*/
+        SubcategoriaDto subcategoriaDto = new SubcategoriaDto( 1);
+        preguntaDto.setSubcategoriaDto( subcategoriaDto );
+        /*Usuario*/
+        UsuarioDto usuarioDto = new UsuarioDto(8);
+        preguntaDto.setUsuarioDto( usuarioDto );
+        PreguntaDto resultado = servicio.addPregunta( preguntaDto );
+        Assert.assertNotEquals( resultado.getId(), 0 );
+    }
+    @Test
+    public void updateCategoria() throws Exception{
+        ucab.dsw.servicio.PreguntaORMWS servicio = new ucab.dsw.servicio.PreguntaORMWS();
+        PreguntaDto preguntaDto = new PreguntaDto();
+        preguntaDto.setPregunta("Pregunta Actualizada");
+        preguntaDto.setFechacreacion("2020/03/28");
+        preguntaDto.setEstatus("A");
+        PreguntaDto resultado = servicio.updatePregunta(2,preguntaDto);
+        Assert.assertNotEquals(resultado.getId(),0);
+    }
+
+    @Test
+    public void deletePregunta() throws Exception{
+        ucab.dsw.servicio.PreguntaORMWS servicio = new ucab.dsw.servicio.PreguntaORMWS();
+        PreguntaDto resultado = servicio.deletePregunta(2);
+        Assert.assertNotEquals(resultado.getId(),0);
+    }
+
+    @Test
+    public void getPregunta() throws Exception{
+        ucab.dsw.servicio.PreguntaORMWS servicio = new ucab.dsw.servicio.PreguntaORMWS();
+        List<Pregunta> preguntas = servicio.getPregunta();
+        Assert.assertFalse("Consulta preguntas",preguntas.isEmpty());
+
+    }
+}
