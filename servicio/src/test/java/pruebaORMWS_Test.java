c83d7977e945966a8d4b0aab01b5f4eb5c98c27a
pancho28 <franciscobravo890@gmail.com>
2020-11-19 22:36:35 -0430

    cambios en el controlador de mysql

--- servicio/src/test/java/pruebaORMWS_Test.java
+++ servicio/src/test/java/pruebaORMWS_Test.java
@@ -10,7 +10,7 @@ import ucab.dsw.entidades.Usuario;
 public class pruebaORMWS_Test
 {
 
-    /*@Test
+    @Test
     public void addUserTest() throws Exception
     {
         ucab.dsw.servicio.pruebaORMWS servicio = new ucab.dsw.servicio.pruebaORMWS();
@@ -19,11 +19,11 @@ public class pruebaORMWS_Test
         usuarioDto.setApellido( "Ponce" );
         usuarioDto.setCorreoelectronico( "bismarckpm@gmail.com" );
         usuarioDto.setEstatus( "A" );
-        TipoUsuarioDto tipoUsuario = new TipoUsuarioDto( 1);
+        TipoUsuarioDto tipoUsuario = new TipoUsuarioDto(5);
         usuarioDto.setTipoUsuarioDto( tipoUsuario );
         UsuarioDto resultado = servicio.addUser( usuarioDto );
         Assert.assertNotEquals( resultado.getId(), 0  );
-    }*/
+    }
 
 
 }
