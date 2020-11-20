d819701ab0766c8da2de3f63c48f5f9f3a251e14
Frank Hesse <hyperschnell11@outlook.sk>
2020-11-20 17:54:57 -0400

    bye

--- servicio/src/main/java/ucab/dsw/servicio/pruebaORMWS.java
+++ servicio/src/main/java/ucab/dsw/servicio/pruebaORMWS.java
@@ -5,15 +5,11 @@ import ucab.dsw.entidades.TipoUsuario;
 import ucab.dsw.entidades.Usuario;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 
 @Path( "/prueba" )
@@ -46,11 +42,50 @@ public class pruebaORMWS extends AplicacionBase
         return resultado;
     }
 
+    @PUT
+    @Path( "/edituser/{id}" )
+    public Usuario editUser(@PathParam("id") int id, UsuarioDto usuarioDto )
+    {
+        DaoUsuario dao = new DaoUsuario();
+        Usuario usuario = dao.find(usuarioDto.getId(), Usuario.class);
+        try
+        {
+            usuario.set_nombre( usuarioDto.getNombre() );
+            usuario.set_apellido( usuarioDto.getApellido() );
+            usuario.set_estatus( usuarioDto.getEstatus() );
+            usuario.set_correoelectronico( usuarioDto.getCorreoelectronico() );
+            TipoUsuario tipoUsuario = new TipoUsuario(usuarioDto.getTipoUsuarioDto().getId());
+            usuario.set_tipousuario( tipoUsuario );
+            Usuario resul = dao.update(usuario);
+        }
+        catch ( Exception ex )
+        {
+            String problema = ex.getMessage();
+        }
+        return usuario;
+    }
+
+    @DELETE
+    @Path( "/deleteuser/{id}" )
+    public Usuario deleteUser(@PathParam("id") int id)
+    {
+        DaoUsuario dao = new DaoUsuario();
+        Usuario usuario = dao.find((long) id, Usuario.class);
+        try
+        {
+            Usuario resul = dao.delete(usuario);
+        }
+        catch ( Exception ex )
+        {
+            String problema = ex.getMessage();
+        }
+        return usuario;
+    }
+
     @GET
     @Path( "/consulta" )
-    @Produces(value = MediaType.TEXT_PLAIN)
-    public String consulta()
-    {
-        return "Epa";
+    public List<Usuario> consulta(){
+        DaoUsuario dao = new DaoUsuario();
+        return dao.findAll(Usuario.class);
     }
 }
