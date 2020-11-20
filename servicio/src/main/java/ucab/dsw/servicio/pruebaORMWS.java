74f2dc07a62e5e82d6716d4615cede0bb25ca5db
GreggSpinetti <50221976+GreggSpinetti@users.noreply.github.com>
2020-11-20 18:46:59 -0400

    Añadida la transacción consultar en Usuario y TipoUsuario, con sus respectivas pruebas.

--- servicio/src/main/java/ucab/dsw/servicio/pruebaORMWS.java
+++ servicio/src/main/java/ucab/dsw/servicio/pruebaORMWS.java
@@ -1,11 +1,21 @@
 package ucab.dsw.servicio;
+import ucab.dsw.accesodatos.DaoCategoria;
+import ucab.dsw.accesodatos.DaoSubCategoria;
 import ucab.dsw.accesodatos.DaoUsuario;
+import ucab.dsw.accesodatos.DaoOpcion;
+import ucab.dsw.accesodatos.DaoSolicitud;
 import ucab.dsw.dtos.UsuarioDto;
-import ucab.dsw.entidades.TipoUsuario;
-import ucab.dsw.entidades.Usuario;
+import ucab.dsw.dtos.CategoriaDto;
+import ucab.dsw.dtos.SubCategoriaDto;
+import ucab.dsw.dtos.OpcionDto;
+import ucab.dsw.dtos.SolicitudDto;
+import ucab.dsw.entidades.*;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 
 @Path( "/prueba" )
@@ -14,7 +24,7 @@ import java.util.List;
 public class pruebaORMWS extends AplicacionBase
 {
     @PUT
-    @Path( "/addUser" )
+    @Path( "/adduser" )
     public UsuarioDto addUser( UsuarioDto usuarioDto )
     {
         UsuarioDto resultado = new UsuarioDto();
@@ -39,53 +49,9 @@ public class pruebaORMWS extends AplicacionBase
     }
 
     @GET
-    @Path("/getUsers")
-    public List<Usuario> getUsers() {
-
-        List<Usuario> usuarios = null;
-        try {
-            DaoUsuario dao = new DaoUsuario();
-            usuarios = dao.findAll(Usuario.class);
-        } catch (Exception ex) {
-            String problema = ex.getMessage();
-        }
-        return usuarios;
-    }
-
-    @PUT
-    @Path( "/updateUser/{id}" )
-    public UsuarioDto updateUsuario(@PathParam("id") long id, UsuarioDto usuarioDto)
+    @Path( "/consulta" )
+    public String consulta()
     {
-        UsuarioDto resultado = new UsuarioDto();
-        try {
-            DaoUsuario dao = new DaoUsuario();
-            Usuario usuario = dao.find(id, Usuario.class);
-            usuario.set_nombre(usuarioDto.getNombre());
-            usuario.set_apellido(usuarioDto.getApellido());
-            usuario.set_correoelectronico(usuarioDto.getCorreoelectronico());
-            Usuario resul = dao.update(usuario);
-            resultado.setId(resul.get_id());
-        } catch (Exception ex) {
-            String problema = ex.getMessage();
-        }
-        return resultado;
-    }
-
-    @DELETE
-    @Path( "/deleteUser/{id}" )
-    public UsuarioDto deleteUser(@PathParam("id") long id)
-    {
-        UsuarioDto resultado = new UsuarioDto();
-        try
-        {
-            DaoUsuario dao = new DaoUsuario();
-            Usuario usuario = dao.find(id, Usuario.class);
-            Usuario resul = dao.delete(usuario);
-            resultado.setId(resul.get_id());
-        }
-        catch (Exception ex){
-            String problema = ex.getMessage();
-        }
-        return resultado;
+        return "Epa";
     }
 }
--- servicio/src/test/java/pruebaORMWS_Test.java
+++ servicio/src/test/java/pruebaORMWS_Test.java
@@ -2,18 +2,25 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import ucab.dsw.accesodatos.DaoSubCategoria;
 import ucab.dsw.dtos.TipoUsuarioDto;
 import ucab.dsw.dtos.UsuarioDto;
-import ucab.dsw.entidades.Usuario;
+import ucab.dsw.dtos.CategoriaDto;
+import ucab.dsw.dtos.SubCategoriaDto;
+import ucab.dsw.dtos.OpcionDto;
+import ucab.dsw.dtos.SolicitudDto;
+import ucab.dsw.entidades.*;
 
-import java.util.List;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
 
 
 public class pruebaORMWS_Test
 {
 
     @Test
-    public void addUserTest() throws Exception {
+    public void addUserTest() throws Exception
+    {
         ucab.dsw.servicio.pruebaORMWS servicio = new ucab.dsw.servicio.pruebaORMWS();
         UsuarioDto usuarioDto = new UsuarioDto();
         usuarioDto.setNombre( "Bismarck" );
@@ -23,37 +30,7 @@ public class pruebaORMWS_Test
         TipoUsuarioDto tipoUsuario = new TipoUsuarioDto( 1);
         usuarioDto.setTipoUsuarioDto( tipoUsuario );
         UsuarioDto resultado = servicio.addUser( usuarioDto );
-        Assert.assertNotEquals( resultado.getId(), 0 );
+        Assert.assertNotEquals( resultado.getId(), 0  );
     }
-
-    @Test
-    public void getUsersTest()  throws  Exception
-    {
-        ucab.dsw.servicio.pruebaORMWS servicio = new ucab.dsw.servicio.pruebaORMWS();
-        List<Usuario> usuarios = servicio.getUsers();
-        Assert.assertFalse("Consulta Usuario",usuarios.isEmpty());
-    }
-
-    @Test
-    public void updateUserTest() throws Exception {
-        ucab.dsw.servicio.pruebaORMWS servicio = new ucab.dsw.servicio.pruebaORMWS();
-        UsuarioDto usuarioDto = new UsuarioDto();
-        usuarioDto.setNombre( "Gregg" );
-        usuarioDto.setApellido( "Spinetti" );
-        usuarioDto.setCorreoelectronico( "greggspinetti@gmail.com" );
-        usuarioDto.setEstatus( "A" );
-        UsuarioDto resultado = servicio.updateUsuario(11,usuarioDto );
-        Assert.assertNotEquals( resultado.getId(), 1);
-    }
-
-    @Test
-    public void deleteUsuarioTest() throws Exception{
-
-        ucab.dsw.servicio.pruebaORMWS servicio = new ucab.dsw.servicio.pruebaORMWS();
-        UsuarioDto resultado = servicio.deleteUser(10);
-        Assert.assertNotEquals( resultado.getId(), 1);
-
-    }
-
 
 }
